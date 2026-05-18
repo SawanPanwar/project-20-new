8b721913691c9d7237fc86afa925b4217793de84
SawanPanwar <skpanwar25@gmail.com>
2026-05-18 11:38:59 +0530

    ok

--- SOS_django_projects/ORSAPI/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class OrsapiConfig(AppConfig):
-    name = 'ORSAPI'
--- SOS_django_projects/service/service/EmailService.py
+++
@@ -1,7 +0,0 @@
-from django.core.mail import send_mail
-
-class EmailService:
-
-    @staticmethod
-    def send(msg):
-        send_mail(msg.subject, msg.text, msg.frm, msg.to, fail_silently=False, html_message=msg.text)
